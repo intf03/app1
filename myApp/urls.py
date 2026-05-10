64496bb13d2b0318d0c3d43906f6b5cacb3fb65d
intf03 <2539122166@qq.com>
2026-05-11 00:03:51 +0800

    register product list api route

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from myApp import views
+
 urlpatterns = [
-    path('screenData/', views.screenData, name='screenData')
+    path('screenData/', views.screenData, name='screenData'),
+    path('productList/', views.productList, name='productList'),
 ]
