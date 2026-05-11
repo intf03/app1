5cee803517125be2936b9145c158e17339bc4a41
intf03 <2539122166@qq.com>
2026-05-11 20:28:35 +0800

    添加机器学习预测接口路由

--- myApp/urls.py
+++ myApp/urls.py
@@ -4,6 +4,7 @@ from myApp import views
 urlpatterns = [
     path('screenData/', views.screenData, name='screenData'),
     path('productList/', views.productList, name='productList'),
+    path('mlPredict/', views.mlPredict, name='mlPredict'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('changePassword/', views.changePassword, name='changePassword'),
